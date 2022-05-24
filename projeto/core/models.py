ce93b26ab8c63c8e3d9255ddb00d38727998b6bf
Hélio Santos <helio.santos@gmail.com>
2022-05-24 15:10:11 -0400

    Criar App Core
    Criado App Core.

---
+++ projeto/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class TimeStampedModel(models.Model):
+    created = models.DataTimeFiled(
+        'criado em',
+        auto_now_add=True,
+        auto_now=False
+    )
+    modified = models.DataTimeField(
+        'modificado em',
+        auto_now_add=False,
+        auto_now=True
+    )
+
+    class Meta:
+        abstract = True
