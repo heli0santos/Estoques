ce0a626617a05c372aa735fdd5e07cfe3941e284
Hélio Santos <helio.santos@gmail.com>
2022-07-18 16:51:19 -0400

    produto form

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 from .models import Produto
+from .forms import ProdutoForm
 
 # Create your views here.
 
@@ -18,3 +20,8 @@ def produto_detail(request, pk):
 def produto_add(request):
     template_name = 'produto_form.html'
     return render(request, template_name)
+
+class ProdutoCreate(CreateView):
+    model = Produto
+    template_name = 'produto_form.html'
+    form_class = ProdutoForm
